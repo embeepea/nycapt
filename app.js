e77307b8af978d8fcd197ddc50cf9b8b07c7d7b7
Mark Phillips <embeepea@git>
2014-01-29 00:35:13 -0500

    more work

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
 var port_number = 3000;
 
 var databaseUrl = "test";
-var collections = ["filterPolygons"];
+var collections = ["filterPolygons", "postings", "edits"];
 var mongodb = require('mongodb');
+var _ = require('underscore');
 var db = require("mongojs").connect(databaseUrl, collections);
+
+function isPointInPoly(poly, pt){
+    for(var c = false, i = -1, l = poly.length, j = l - 1; ++i < l; j = i)
+        ((poly[i][0] <= pt[0] && pt[0] < poly[j][0]) || (poly[j][0] <= pt[0] && pt[0] < poly[i][0]))
+        && (pt[1] < (poly[j][1] - poly[i][1]) * (pt[0] - poly[i][0]) / (poly[j][0] - poly[i][0]) + poly[i][1])
+        && (c = !c);
+    return c;
+}
+
+function polygonFilter(polygons) {
+    return function(posting) {
+        var i;
+        for (i=0; i<polygons.length; ++i) {
+            if (isPointInPoly(polygons[i].coords, [posting.Latitude,posting.Longitude])) {
+                return true;
+            }
+        }
+        return false;
+    };
+}
+
+function postingPredicate(posting) {
+    if (posting.PostingTitle.match(/superbowl/gi)) { return false; }
+    if (posting.PostingTitle.match(/super\s+bowl/gi)) { return false; }
+    return true;
+}
 
 var express = require('express')
   , util = require('util')
@@ -47,20 +74,35 @@ app.get('/filter', function(req, res){
 });
 
 app.post('/addpolygon', function(req, res) {
-    console.log('got polygon: ' + JSON.stringify(req.body));
-    db.filterPolygons.save({
+     db.filterPolygons.save({
         'coords' : req.body
     }, function(err, value) {
-        console.log('saved polygon to mongo');
         res.send(JSON.stringify({'status' : 'OKyes'}));
     });
 });
-app.post('/polygons', function(req, res) {
+app.post('/filterPolygons', function(req, res) {
     db.filterPolygons.find(function(err,filterPolygons) {
         res.send(JSON.stringify(filterPolygons));
     });
 });
-
+app.get('/postings', function(req, res) {
+    db.filterPolygons.find(function(err,filterPolygons) {
+        db.postings.find(function(err,postings) {
+            postings = _.filter(postings, polygonFilter(filterPolygons));
+            postings = _.filter(postings, postingPredicate);
+            db.edits.findOne(function(err,edits) {
+                if (edits) {
+                    _.each(postings, function(posting) {
+                        if (edits[posting]) {
+                            _.extend(posting, edits);
+                        }
+                    });
+                }
+                res.send(JSON.stringify(postings));
+            });
+        });
+    });
+});
 
 console.log('listening on port ' + port_number);
 
--- public/map.js
+++ public/map.js
@@ -3,7 +3,7 @@
 /*    var cloudmade_api_key = "a5b0d6c02084a00bd522ad1ee48e37f"; */
     var cloudmade_api_key = "8ee2a50541944fb9bcedded5165f09d9";
 
-    var subway_routes_to_show = ['L', 'Q', 'R', 'G', 'J'];
+    var subway_routes_to_show = ['L', 'Q', 'R', 'G', 'J', '2', '5'];
 
     function is_station_on_shown_subway_route(station) {
         var i;
@@ -29,53 +29,79 @@
 
         var layerControl = L.control.layers().addTo(map);
 
-                $.ajax({
-                    type: 'POST',
-                    //data: JSON.stringify(data),
-                    contentType: 'application/json',
-                    url: '/polygons',
-                        success: function(filterPolygons) {
-                            var polys = JSON.parse(filterPolygons);
-                            var polyLayers = [];
-                            _.each(polys, function(polygon) {
-                                polyLayers.push(L.polygon(polygon.coords));
-                            });
-                            var polyLayerGroup = L.layerGroup(polyLayers).addTo(map);
-                            layerControl.addOverlay(polyLayerGroup, "Filter Polygons");
-                        }
-                    });
+        function createFilterPolygonsLayer(filterPolygons) {
+            var polyLayers = [];
+            _.each(filterPolygons, function(polygon) {
+                polyLayers.push(L.polygon(polygon.coords));
+            });
+            layerControl.addOverlay(L.layerGroup(polyLayers).addTo(map), "Filter Polygons");
+        }
 
-                $.ajax({
-                    type: 'GET',
-                    //data: JSON.stringify(data),
-                    contentType: 'application/json',
-                    url: '/stations.json',
-                        success: function(stations) {
-//console.log(data);
-//                            var stations = JSON.parse(data);
-                            var stationCircles = [];
-                            _.each(stations, function(station) {
-                                if (is_station_on_shown_subway_route(station)) {
-                                    var stationCircle = L.circle([station.lat,station.lng], 10, {
-                                        color : '#f00',
-                                        weight: 2,
-                                        fillColor: '#f03',
-                                        fillOpacity: 0.5
-                                    }).bindPopup(L.popup()
-                                                 .setLatLng(L.latLng([station.lat,station.lng]))
-                                                 .setContent(Mustache.render($('#stationpopup').text(), {
-                                                     'name' : station.nm,
-                                                     'routes' : station.rts.join(", ")
-                                                 })));
-                                    stationCircles.push(stationCircle);
-                                }
-                            });
-                            var stationsLayerGroup = L.layerGroup(stationCircles).addTo(map);
 
-                            layerControl.addOverlay(stationsLayerGroup, "Subway Stations");
-                        }
-                    });
+        $.ajax({
+            type: 'POST',
+            contentType: 'application/json',
+            url: '/filterPolygons',
+            success: function(filterPolygons) {
+                createFilterPolygonsLayer(JSON.parse(filterPolygons));
+            }
+        });
 
+        function createStationsLayer(stations) {
+            var stationCircles = [];
+            _.each(stations, function(station) {
+                if (is_station_on_shown_subway_route(station)) {
+                    var stationCircle = L.circle([station.lat,station.lng], 10, {
+                        color : '#f00',
+                        weight: 2,
+                        fillColor: '#f03',
+                        fillOpacity: 0.5
+                    }).bindPopup(Mustache.render($('#stationpopup').text(), {
+                        'name' : station.nm,
+                        'routes' : station.rts.join(", ")
+                    }));
+                    stationCircles.push(stationCircle);
+                }
+            });
+            layerControl.addOverlay(L.layerGroup(stationCircles).addTo(map), "Subway Stations");
+        }
+
+        $.ajax({
+            type: 'GET',
+            contentType: 'application/json',
+            url: '/stations.json',
+            success: function(stations) {
+                createStationsLayer(stations);
+            }
+        });
+
+
+        function createPostingsLayer(postings) {
+            var postingMarkers = [];
+            _.each(postings, function(posting) {
+                var stationMarker = L.marker([posting.Latitude,posting.Longitude], {
+                    title : posting.PostingTitle
+                }).bindPopup(Mustache.render($('#postingpopup').text(), {
+                    "ask"          : posting.Ask,
+                    "bedrooms"     : posting.Bedrooms,
+                    "imagethumb"   : posting.ImageThumb ? ('<img src="' + posting.ImageThumb + '">') : "",
+                    "posteddate"   : (new Date(parseInt(posting.PostedDate)*1000)).toString(),
+                    "postingtitle" : posting.PostingTitle,
+                    "postingurl"   : "http://newyork.craigslist.org" + posting.PostingURL
+                }));
+                postingMarkers.push(stationMarker);
+            });
+            layerControl.addOverlay(L.layerGroup(postingMarkers).addTo(map), "Postings");
+        }
+
+        $.ajax({
+            type: 'GET',
+            contentType: 'application/json',
+            url: '/postings',
+            success: function(postings) {
+                createPostingsLayer(JSON.parse(postings));
+            }
+        });
 
         var messageAreaVisible;
 
